67a52c570510ca57c605c9ea92d13b789a490be9
Choi Seoyeon <77572658+csy-59@users.noreply.github.com>
2025-01-31 23:43:58 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+    
+    while(n >= a)
+    {
+        int newBottle = n / a * b;
+        answer += newBottle;
+        n = newBottle + n % a;
+    }
+    
+    return answer;
+}
