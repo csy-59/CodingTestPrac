1356b5247cd721e947683d5ca880d61395c9380a
Choi Seoyeon <77572658+csy-59@users.noreply.github.com>
2026-02-08 17:40:04 +0900

    [Gold V] Title: 빗물, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int h, w;
+    cin >> h >> w;
+
+    vector<int> walls(w, 0);
+
+    int result = 0;
+    int wallStartIndex = -1;
+    int currentWallHeight = 0;
+    for (int i = 0; i < w; ++i)
+    {
+        cin >> walls[i];
+    }
+
+    for (int i = h; i >= 0; --i)
+    {
+        vector<int> pos;
+
+        for (int j = 0; j < w; ++j)
+        {
+            if (walls[j] >= i)
+                pos.push_back(j);
+        }
+
+        if (pos.size() <= 1)
+            continue;
+
+        for (int j = 0, size = pos.size(); j < size - 1; ++j)
+        {
+            result += pos[j + 1] - pos[j] - 1;
+        }
+    }
+
+    cout << result;
+
+	return 0;
+}
