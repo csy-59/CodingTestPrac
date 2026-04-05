98920411e25b1ea91498d6b1a6c1bfda25f6c295
Choi Seoyeon <77572658+csy-59@users.noreply.github.com>
2026-04-05 23:49:11 +0900

    [Gold II] Title: 새로운 게임 2, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.cc
+++ 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.cc
@@ -1,83 +1,203 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <deque>
+#include <vector>
+#include <string>
+
 using namespace std;
 
-int n, k;
-int color[12][12];                 
-vector<int> board[12][12];         
-int x[10], y[10], dir[10];         
+pair<int, int> moveWay[4] = {
+	make_pair(0, 1),
+	make_pair(0, -1),
+	make_pair(-1, 0),
+	make_pair(1, 0)
+};
 
-int dx[4] = {0, 0, -1, 1};
-int dy[4] = {1, -1, 0, 0};
+class Piece;
 
-int reverseDir(int d)
-{
-    if (d == 0) return 1;
-    if (d == 1) return 0;
-    if (d == 2) return 3;
-    return 2;
-}
+int n;
+int map[12][12] = { 0, };
+Piece* PiecePos[12][12] = { nullptr };
+
+class Piece {
+public:
+	int num;
+	Piece* top;
+	Piece* bottom;
+	pair<int, int> pos;
+	int dir;
+
+	Piece()
+	{
+		pos = make_pair(0, 0);
+		dir = -1;
+		num = -1;
+		top = bottom = nullptr;
+	}
+
+	void Set(int x, int y, int d, int Number)
+	{
+		pos.first = x - 1;
+		pos.second = y - 1;
+		dir = d - 1;
+		num = Number;
+	}
+
+	int Move()
+	{
+		auto nPos = make_pair(pos.first + moveWay[dir].first, pos.second + moveWay[dir].second);
+		bool isRed = false;
+
+		// 파랑
+		if (nPos.first < 0 || nPos.first >= n ||
+			nPos.second < 0 || nPos.second >= n ||
+			map[nPos.first][nPos.second] == 2)
+		{
+			Flip();
+
+			auto nnPos = make_pair(pos.first + moveWay[dir].first, pos.second + moveWay[dir].second);
+			bool cannotMove = nnPos.first < 0 || nnPos.first >= n ||
+				nnPos.second < 0 || nnPos.second >= n ||
+				map[nnPos.first][nnPos.second] == 2;
+
+			if (cannotMove == true)
+			{
+				return PiecePos[pos.first][pos.second]->GetCurrentStack();
+			}
+
+			nPos = nnPos;
+		}
+
+		// 이동
+		if (bottom == nullptr) PiecePos[pos.first][pos.second] = nullptr;
+		else 
+		{
+			bottom->top = nullptr; bottom = nullptr;
+		}
+
+		pos = nPos;
+		
+		auto nBottom = PiecePos[pos.first][pos.second];
+		if (nBottom == nullptr)
+		{
+			PiecePos[pos.first][pos.second] = this;
+			if (bottom != nullptr) bottom->top = nullptr;
+			bottom = nullptr;
+		}
+		else
+		{
+			bottom = nBottom->GetSuperTop();
+			bottom->top = this;
+		}
+
+		if (top != nullptr) 
+			top->Move(dir);
+		
+		if (map[pos.first][pos.second] == 1)
+		{
+			if (PiecePos[pos.first][pos.second] == this)
+			{
+				PiecePos[pos.first][pos.second] = GetSuperTop();
+			}
+
+			auto b = bottom; auto t = GetSuperTop(); bottom = nullptr;
+			if (b != nullptr) b->top = nullptr;
+			Reverse();
+			if(b != nullptr) b->top = t;
+			t->bottom = b;
+		}
+
+		return PiecePos[pos.first][pos.second]->GetCurrentStack();
+	}
+
+	void Move(int parentDir)
+	{
+		pos.first += moveWay[parentDir].first;
+		pos.second += moveWay[parentDir].second;
+
+		if (top != nullptr) top->Move(parentDir);
+	}
+
+	void Flip()
+	{
+		switch (dir)
+		{
+		case 0: dir = 1; break;
+		case 1: dir = 0; break;
+		case 2: dir = 3; break;
+		case 3: dir = 2; break;
+		}
+	}
+
+	void Reverse()
+	{
+		if (top != nullptr)
+			top->Reverse();
+
+		auto t = bottom;
+		bottom = top;
+		top = t;
+	}
+
+	Piece* GetSuperBottom()
+	{
+		return (bottom != nullptr) ? bottom->GetSuperBottom() : this;
+	}
+
+	Piece* GetSuperTop()
+	{
+		return (top != nullptr) ? top->GetSuperTop() : this;
+	}
+
+	int GetCurrentStack()
+	{
+		return GetSuperBottom()->GetStackSize();
+	}
+
+	int GetStackSize()
+	{
+		return top != nullptr ? top->GetStackSize() + 1 : 1;
+	}
+};
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
 
-    cin >> n >> k;
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j)
-            cin >> color[i][j];
+	int k;
+	cin >> n >> k;
 
-    for (int i = 0; i < k; ++i)
-    {
-        cin >> x[i] >> y[i] >> dir[i];
-        x[i]--; y[i]--; dir[i]--;
-        board[x[i]][y[i]].push_back(i);
-    }
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			cin >> map[i][j];
+		}
+	}
 
-    for (int turn = 1; turn <= 1000; ++turn)
-    {
-        for (int i = 0; i < k; ++i)
-        {
-            int cx = x[i], cy = y[i];
-            int d = dir[i];
+	vector<Piece> pieces(k);
+	for (int i = 0; i < k; ++i)
+	{
+		int x, y, d;
+		cin >> x >> y >> d;
+		pieces[i].Set(x, y, d, i + 1);
+		PiecePos[x - 1][y - 1] = &pieces[i];
+	}
 
-            auto &curStack = board[cx][cy];
-            int idx = find(curStack.begin(), curStack.end(), i) - curStack.begin();
+	int time = 1;
+	while (time <= 1000)
+	{
+		for (int i = 0; i < k; ++i)
+		{
+			if (pieces[i].Move() >= 4)
+			{
+				cout << time;
+				return 0;
+			}
+		}
+ 		++time;
+	}
 
-            int nx = cx + dx[d];
-            int ny = cy + dy[d];
-
-            if (nx < 0 || nx >= n || ny < 0 || ny >= n || color[nx][ny] == 2)
-            {
-                dir[i] = d = reverseDir(d);
-                nx = cx + dx[d];
-                ny = cy + dy[d];
-
-                if (nx < 0 || nx >= n || ny < 0 || ny >= n || color[nx][ny] == 2)
-                    continue;
-            }
-
-            vector<int> moving(curStack.begin() + idx, curStack.end());
-            curStack.erase(curStack.begin() + idx, curStack.end());
-
-            if (color[nx][ny] == 1)
-                reverse(moving.begin(), moving.end());
-
-            for (int m : moving)
-            {
-                x[m] = nx;
-                y[m] = ny;
-                board[nx][ny].push_back(m);
-            }
-
-            if (board[nx][ny].size() >= 4)
-            {
-                cout << turn;
-                return 0;
-            }
-        }
-    }
-
-    cout << -1;
-    return 0;
+	cout << -1;
+	return 0;
 }
