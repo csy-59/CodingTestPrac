d76f40f57d59d2e0c1c8e060640db3675854aa07
Choi Seoyeon <77572658+csy-59@users.noreply.github.com>
2026-04-13 15:16:34 +0900

    [Silver III] Title: 주유소, Time: 20 ms, Memory: 3196 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -3,22 +3,6 @@
 
 using namespace std;
 
-unsigned long long FindLow(vector<unsigned long long>& v, int endPos)
-{
-    unsigned long long min = v[0];
-    int index = 0;
-    for (int i = 0; i < endPos; ++i)
-    {
-        if (min > v[i])
-        {
-            min = v[i];
-            index = i;
-        }
-    }
-
-    return index;
-}
-
 int main()
 {
     ios::sync_with_stdio(false);
@@ -33,36 +17,24 @@ int main()
         cin >> roads[i];
     }
 
-    vector<unsigned long long> price(n, 0);
+    vector<int> price(n, 0);
     for (int i = 0; i < n; ++i)
     {
         cin >> price[i];
     }
 
     unsigned long long result = 0;
-    int l = FindLow(price, n - 1);
-    int end = n - 2;
-    while (l > 0)
+    unsigned long long low = price[0];
+
+    for (int i = 0; i < n - 1; ++i)
     {
-        unsigned long long dist = 0;
-        for (int i = end; i >= l; --i)
+        if (price[i] < low)
         {
-            dist += roads[i];
+            low = price[i];
         }
 
-        result += dist * price[l];
-
-        end = l - 1;
-        l = FindLow(price, l);
+        result += low * roads[i];
     }
-
-    unsigned long long dist = 0;
-    for (int i = end; i >= l; --i)
-    {
-        dist += roads[i];
-    }
-
-    result += dist * price[l];
 
     cout << result;
 
