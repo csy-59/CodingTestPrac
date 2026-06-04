45093ca463e523fc33dfc75b87511564d78d5ea4
Choi Seoyeon <77572658+csy-59@users.noreply.github.com>
2026-06-04 21:09:49 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 3.82 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    
+    int jobCount = progresses.size();
+    for(int i = 0; i < jobCount; ++i)
+    {
+        if(progresses[i] >= 100) continue; // 이미 배포 완료
+        
+        int finDate = (int) ceil((100 - progresses[i]) / (float)speeds[i]);
+        int finCount = 0;
+        bool preJobDone = true;
+        for(int j = i; j < jobCount; ++j)
+        {
+            progresses[j] += speeds[j] * finDate;
+            if(preJobDone == true && progresses[j] >= 100)
+                ++finCount;
+            else
+                preJobDone = false;
+        }
+        answer.push_back(finCount);
+    }
+    
+    return answer;
+}
