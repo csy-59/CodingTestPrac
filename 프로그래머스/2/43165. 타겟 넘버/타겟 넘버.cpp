f5a9d7854bd596e4a5c7747de8a259979b2d6cd0
Choi Seoyeon <77572658+csy-59@users.noreply.github.com>
2026-06-16 20:45:13 +0900

    [level 2] Title: 타겟 넘버, Time: 3.10 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,31 +1,23 @@
 #include <string>
 #include <vector>
-#include <stack>
 
 using namespace std;
 
+int s = 0;
+int t;
+
+int dfs(vector<int>& nums, int index, int total)
+{
+    if(index == s)
+        return total == t ? 1 : 0;
+    
+    return dfs(nums, index + 1, total + nums[index]) +
+        dfs(nums, index + 1, total - nums[index]);
+}
+
 int solution(vector<int> numbers, int target) {
-    int count = 0;
+    t = target;
+    s = numbers.size();
     
-    int size = numbers.size();
-    
-    // 다음 인덱스, 지금까지 총합
-    stack<pair<int, int>> s;
-    s.push({0, 0});
-    
-    while(s.empty() == false)
-    {
-        pair<int, int> temp = s.top(); s.pop();
-        
-        if(temp.first == size)
-        {
-            if(temp.second == target) ++count;
-            continue;
-        }
-        
-        s.push({temp.first + 1, temp.second + numbers[temp.first]});
-        s.push({temp.first + 1, temp.second - numbers[temp.first]});
-    }
-    
-    return count;
+    return dfs(numbers, 0, 0);
 }
