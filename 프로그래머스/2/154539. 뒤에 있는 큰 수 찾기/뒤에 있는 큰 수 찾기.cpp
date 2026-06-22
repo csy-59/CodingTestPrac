0a7e98c7c483f44b95f005a1f2a236135823257c
Choi Seoyeon <77572658+csy-59@users.noreply.github.com>
2026-06-22 19:50:45 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 329.96 ms, Memory: 240 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    int n = numbers.size();
+    vector<int> answer(n, -1);
+    stack<int> st; // 인덱스 저장
+
+    for(int i = 0; i < n; ++i)
+    {
+        while(!st.empty() && numbers[st.top()] < numbers[i])
+        {
+            answer[st.top()] = numbers[i];
+            st.pop();
+        }
+
+        st.push(i);
+    }
+
+    return answer;
+}
